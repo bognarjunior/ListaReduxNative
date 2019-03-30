b011040aea911f09dc106ed4cf055cb0590405f3
Bognar Junior <bognar_junior@yahoo.com.br>
2019-03-29 22:51:31 -0300

    Import redux and react-reducer and create reducer
    Importando o redux e o react-redux e criando o reducer

--- App.js
+++ App.js
@@ -7,13 +7,20 @@
  */
 
 import React, {Component} from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducer';
 import Main from './components/Main';
+
+const store = createStore(reducer);
 
 export default class App extends Component {
   render() {
-
     return (
-      <Main />
+      <Provider store={store}>
+        <Main />
+      </Provider>
     );
   }
 }
