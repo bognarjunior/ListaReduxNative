53327141b50a17bffaee1320285a909b072b5d4d
Bognar Junior <bognar_junior@yahoo.com.br>
2019-04-01 23:43:56 -0300

    Configurate Redux Saga
    Configurando o redux-saga

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducer';
 import Main from './components/Main';
 
-const store = createStore(reducer);
+import sagas from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(sagas)
 
 export default class App extends Component {
   render() {
---
+++ sagas.js
@@ -0,0 +1,5 @@
+function* rootSaga() {
+
+}
+
+export default rootSaga;
